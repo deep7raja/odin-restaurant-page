7dfbc117af9200fd5d06cbbee0bba77dea9f5486
Global user <global@gmail.com>
2022-02-04 17:55:51 +0530

    added navigation header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import './style.css';
 import {homeDiv} from './page-home.js';
 import {contactDiv} from './page-contact.js'
-import {headerDiv} from './page-header.js';
+import {headerDiv, btnHome, btnContact} from './page-header.js';
 
 const body = document.querySelector('body');
 const contentDiv = document.querySelector('#content');
 
 body.insertBefore(headerDiv, contentDiv)
+contactDiv.style.display = 'none';
 contentDiv.appendChild(homeDiv);
 contentDiv.appendChild(contactDiv);
+
+btnHome.onclick=()=>{
+    homeDiv.style.display = 'flex';
+    btnHome.classList.add('btnActive');
+    contactDiv.style.display = 'none';
+    btnContact.classList.remove('btnActive');
+}
+btnContact.onclick=()=>{
+    homeDiv.style.display = 'none';
+    btnHome.classList.remove('btnActive');
+    contactDiv.style.display = 'flex';
+    btnContact.classList.add('btnActive');
+}
+
 console.log('-----------index.js');
 console.log({contentDiv});
