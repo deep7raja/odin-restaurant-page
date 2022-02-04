dea8637fbcbf1036f90f0017a6d6ea99faa53696
Global user <global@gmail.com>
2022-02-04 17:03:39 +0530

    contacts page added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './style.css';
 import {homeDiv} from './page-home.js';
+import {contactDiv} from './page-contact.js'
+import {headerDiv} from './page-header.js';
 
+const body = document.querySelector('body');
 const contentDiv = document.querySelector('#content');
 
-
+body.insertBefore(headerDiv, contentDiv)
 contentDiv.appendChild(homeDiv);
+contentDiv.appendChild(contactDiv);
 console.log('-----------index.js');
 console.log({contentDiv});
